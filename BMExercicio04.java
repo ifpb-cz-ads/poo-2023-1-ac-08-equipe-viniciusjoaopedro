3f75d243fff53341f66d8642dfe72a39bde131c1
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-04-30 12:29:43 -0300

    Update BMExercicio04.java

--- BMExercicio04.java
+++ BMExercicio04.java
@@ -6,20 +6,4 @@ public class Equipamento {
         this.nome = nome;
         this.preco = preco;
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getPreco() {
-        return preco;
-    }
-
-    public void setPreco(int preco) {
-        this.preco = preco;
-    }
 }
