95be8b6e9400095c8d310f2df1e28fe770dd5657
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-04-30 12:20:54 -0300

    Create BMExercicio04.java

---
+++ BMExercicio04.java
@@ -0,0 +1,25 @@
+public class Equipamento {
+    private String nome;
+    private int preco;
+
+    public Equipamento(String nome, int preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+}
